f4f266d6fc42bd5e07f98f05eed8f43e75f6ec47
Koltsz <36265128+Koltsz@users.noreply.github.com>
2020-02-02 09:24:20 +0000

    Add files via upload

--- eatcard-install.sh
+++ eatcard-install.sh
@@ -6,12 +6,20 @@ apt-get update && apt-get -y upgrade
 apt-get install xserver-xorg x11-xserver-utils xinit openbox -y
 # Install Chromium browser
 apt-get install chromium-browser -y
-printf "[X] - OS update and software installed"
+printf "
+
+[X] - OS update and software installed
+
+"
 
 # Create .bash_profile to autostart the environmentreboo
 printf 'startx' > /home/user/.bash_profile
 chown user:user /home/user/.bash_profile
-printf "[X] - Automatic app load installed"
+printf "
+
+[X] - Automatic app load installed
+
+"
 
 # Openbox autostart settings
 mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart-backup
@@ -23,11 +31,19 @@ xset -dpms
 xrandr --orientation right
 chromium-browser  --disbale-infobars --disable-translate --disable-breakpad --disable-crash-reporter --disable-sync --kiosk "https://eatcard.nl"
 ' > /etc/xdg/openbox/autostart
-printf "[X] - Screen and browser setup completed"
+printf "
+
+[X] - Screen and browser setup COMPLETED
+
+"
 
 # Openbox Keybindings
 wget https://raw.githubusercontent.com/Koltsz/eatcard/master/rc.xml -P /home/user/.config/openbox/
-printf "[X] - Disabled shorcut keys"
+printf "
+
+[X] - Disabled shorcut keys COMPLETED
+
+"
 
 # Auto Login
 mkdir /etc/systemd/system/getty@tty1.service.d
@@ -36,6 +52,10 @@ printf '
 ExecStart=
 ExecStart=-/sbin/agetty --noissue --autologin user %%I $$TERM
 Type=idle' > /etc/systemd/system/getty@tty1.service.d/override.conf
-printf "[X] - Automatic login installed"
+printf "
+
+[X] - Automatic login installed
+
+"
 
 printf "Reboot for settings to take affect!"
