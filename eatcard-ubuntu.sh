dad0364a14f5c9bfc41cdea53dcbd7af16943fbb
Koltsz <safa@djin.co.uk>
2020-02-05 11:21:10 +0000

    for gui ubuntu

---
+++ eatcard-ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update and upgrade OS
+apt-get update && apt-get -y upgrade
+# Install Chromium browser
+apt-get install chromium-browser -y 
+
+printf "
+[Desktop Entry]
+Version=1.0
+Name=EatCard App
+GenericName=Web Browser
+Exec=/usr/bin/chromium --disable-translate --disable-crash-reporter --kiosk 'https://app.eatcard.nl/account/login'
+" > /usr/share/applications
+
+printf "
+
+[X] - OS update and software installed
+
+"
