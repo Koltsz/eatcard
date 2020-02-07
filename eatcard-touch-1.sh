26906457ddb4bdc3f52da4131353fe54973961dc
Koltsz <safa@djin.co.uk>
2020-02-07 09:08:20 +0000

    Update

--- eatcard-touch-1.sh
+++ eatcard-touch-1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Graphics
+add-apt-repository ppa:oibaf/graphics-drivers
 # Update and upgrade OS
 apt-get update && apt-get -y upgrade
 # Install required software
-apt-get install xserver-xorg x11-xserver-utils xinit openbox -y
+apt-get install xserver-xorg x11-xserver-utils xinit -y
 # Install xinput, xterm and network-manager
 apt-get install xinput xterm network-manager -y
 # INstall intel driver
@@ -12,19 +14,31 @@ apt-get install xserver-xorg-video-intel -y
 apt-get install chromium-browser -y
 # Install gnome session
 apt-get install gnome-session -y
-
+apt-get install gnome-tweaks -y
 
 systemctl disable systemd-networkd-wait-online.service
 systemctl mask systemd-networkd-wait-online.service
+
 printf "
 
 [X] - OS update and software installed
 
 "
 
-# # Openbox autostart settings
-# printf '
-# xterm 
-# ' > /etc/xdg/openbox/autostart
+printf '
+Section "Device"
+   Identifier "Intel Graphics"
+   Driver "intel"
+   Option "AccelMethod" "uxa"
+   Option "TearFree" "true"
+   Option  "TripleBuffer" "true"
+EndSection
+' > /etc/X11/xorg.conf.d/20-intel.conf
 
-# runuser -l user 'startx'
+printf "
+
+[X] - Graphics change
+
+FINISHED - You can reboot now
+
+"
